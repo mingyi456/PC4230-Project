6a1e7dc47343a632197deb21703412ec24662314
mingyi456 <mingyi456@hotmail.com>
2022-11-22 01:32:58 +0800

    Attempt implementing theoretical transition probability

--- main.m
+++ main.m
@@ -10,9 +10,6 @@ function Prob = main(A, w, t_final, target_state_num)
 	gaussian_state_width = 1;
 	gaussian_state = exp(-(X - gaussian_state_centre) .^2 / (2 * gaussian_state_width ^ 2));
 
-	if ~exist('t_final','var')
-      	t_final = 400;
-	end
 	if ~exist('target_state_num','var')
       	target_state_num = 1;
 	end
@@ -20,29 +17,41 @@ function Prob = main(A, w, t_final, target_state_num)
 	initial_state = normalize(hermiteH(0 , X) .* gaussian_state, "norm");
 	target_state = normalize(hermiteH(target_state_num , X) .* gaussian_state, "norm");
 
+	if ~exist('t_final','var')
+      	t_final = 400;
+	end
+
 	dt = 0.005;
 	num_steps = ceil(t_final / dt); T = 1 : num_steps;
 	
-	Prob = zeros(1, num_steps);
-
-
 	U_t = exp(-1i * dt * (P .^ 2 / 2));
 	curr_state = initial_state;
-	
+	Prob = zeros(1, num_steps);
+
 	for i1 = T
 		U_v = exp(((-1i/2) * dt) * ((A * sin(X) * cos(w * i1 * dt)) + (X .^ 2 / 2)));
-	
 		curr_state = U_v .* curr_state;
 		curr_state = U_t .* fft(curr_state);
 		curr_state = U_v .* ifft(curr_state);
-	
 		dot_pdt = target_state * transpose(curr_state);
 		Prob(i1) = dot_pdt * dot_pdt';
 	
 	end
 
+	v_matrix = calc_v_matrix(A);
+	w_nm = target_state_num;
 	
-	plot(T * dt/pi, Prob);
+	if rem(target_state_num, 2) ~= 0
+		v_nm = v_matrix(1, target_state_num + 1);
+	end
+
+	Prob_theo = ((4 * abs(v_nm) ^ 2) / (w - w_nm) ^ 2) * sin((w - w_nm) * T * dt / 2).^2;
+	
+
+			
+	plot(T * dt / pi, Prob);
+	plot(T * dt / pi, Prob_theo);
+	legend(["Simulated Prob", "Theoretical Prob"]);
 	title(sprintf("A = %.3f, w = %.3f for state 0\\rightarrow%i", A, w, target_state_num), FontSize=18);
 	xlabel('t/\pi', FontSize=18);
 	xlim([0,T(end)*dt/pi])
